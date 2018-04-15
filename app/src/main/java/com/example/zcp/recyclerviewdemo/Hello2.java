7b49f6e0a581c629ef515e9433bf0393b1648677
GitHubZhangLie <2529223773@qq.com>
2018-04-15 23:36:16 +0800

    我还是要修改你！！

---
+++ app/src/main/java/com/example/zcp/recyclerviewdemo/Hello2.java
@@ -0,0 +1,12 @@
+package com.example.zcp.recyclerviewdemo;
+
+/**
+ * Created by fireman on 2018/4/15.
+ */
+
+public class Hello2 {
+
+    String name="你好吗";
+    String name2="你好吗";
+    String name3="你好吗";
+}
